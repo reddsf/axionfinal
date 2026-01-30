45ec02d36a53ce95bc00774611ee2c13ce336094
reddsf <bourosalexandru7@gmail.com>
2026-01-30 14:13:43 +0200

    Add files via upload

---
+++ src/main/java/com/axiomendgame/AxiomEndgameMod.java
@@ -0,0 +1,10 @@
+package com.axiomendgame;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AxiomEndgameMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Axiom Endgame initialized");
+    }
+}
